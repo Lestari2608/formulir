c9b731d5481a3348f947f92bc053d112822e0963
Lestari2608 <dewi80322@gmail.com>
2020-07-02 09:32:13 +0700

    form

---
+++ formulir/proses.php
@@ -0,0 +1,26 @@
+<?php
+   echo'Nama:', "&nbsp;", $_GET['fname'];
+   echo "&nbsp;";
+   echo "<br/>";
+	echo'Alamat:', "&nbsp;", $_GET['alamat'];
+   echo "&nbsp;";
+   echo "<br/>";
+   echo'Tempat Lahir:', "&nbsp;", $_GET['tempat'];
+   echo "&nbsp;";
+   echo "<br/>";
+   echo'Jenis Kelamin:', "&nbsp;", $_GET['gendor'];
+   echo "&nbsp;";
+   echo "<br/>";
+   echo'Alamat Web:', "&nbsp;", $_GET['web'];
+   echo "&nbsp;";
+   echo "<br/>";
+   echo'E-mail:', "&nbsp;", $_GET['email'];
+   echo "&nbsp;";
+   echo "<br/>";
+   echo'No.hp:', "&nbsp;", $_GET['hp'];
+   echo "&nbsp;";
+   echo "<br/>";
+   echo'Nilai Ijazah:', "&nbsp;", $_GET['files'];
+   echo "&nbsp;";
+   echo "<br/>";
+   ?>	
